df2d5196f303d1ba6e0d430ed20122684264757b
aidasyafawati304 <asyafawati19@gmail.com>
2021-10-27 14:47:20 +0800

    lab02
    RoundingApplication

---
+++ workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
@@ -0,0 +1,20 @@
+package lab02.app;
+
+
+/**
+ * This application rounding the number to the nearest decimal points.
+ *
+ * @author aida
+ *
+ */
+
+
+public class RoundingApplication {
+	
+	public static void main(String[]args) {
+		System.out.println("Rounding number to the nearest decimal points");
+		
+		
+	}
+
+}
