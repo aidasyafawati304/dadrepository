4a067110f03142d3ddd1a64f101ba0497499d0e2
aidasyafawati304 <asyafawati19@gmail.com>
2021-10-29 00:42:55 +0800

    Exercise 4_DataOutputStream
    Data represent of 2 station
    Data consists of Station ID, Station Name, Rainfall data of 6 Days

---
+++ workspace-dadlabs/lab03/src/lab03/Exercise4_PrimitiveDataGenerator.java
@@ -0,0 +1,48 @@
+package lab03;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+public class Exercise4_PrimitiveDataGenerator {
+	
+public static void main(String[] args) {
+		
+		
+		// 1. Declare output file 
+		String outFile = "dataRainfall.txt";
+		
+		// Data declaration
+		String name[] = Hospital Jasin;
+		String date[] = {"22Oct", "23Oct", "24Oct", "25Oct", "26Oct", "27Oct"};
+		double rainfall[] = {0.0, 0.0, 0.0, 0.0, 0.0, 8.0};
+		
+		try {
+			
+			// 2. Create stream to read data
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream(outFile));
+			
+			// Process data
+			for (int index = 0; index < date.length; index++) {
+				
+				// 3. Write data into data stream
+				dos.writeUTF(name[index]);
+				dos.writeUTF(date[index]);
+				dos.writeDouble(rainfall[index]);
+				
+				// 4. Flush for each writing
+				dos.flush();
+			}
+			
+			// 5. Close stream
+			dos.close();
+			
+		} catch (Exception ex) {
+			
+			ex.printStackTrace();
+		}
+		
+		// Indicate end of program - Could be successful
+		System.out.println("End of program. Check out " + outFile); 
+	}
+
+}
